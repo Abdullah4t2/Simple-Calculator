dacb50e284ac46ab28a21519339eff6de590fb69
Abdullah4t2 <159570855+Abdullah4t2@users.noreply.github.com>
2024-04-06 20:52:06 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+// printing a Wellcome message
+console.log("\n\tWellcome To \'codeWithAbdullah\'-CLI Simple Calculator\n");
+
+
+// Asking question from user through inquirer
+
+const answers = await inquirer. prompt ([
+    {message: "Enter first number", type: "number", name: "firstNumber"}, 
+    {message: "Enter second number", type: "number", name: "secondNumber"},
+    {
+        message: "select one operator to perform operations",
+        type:"list",
+        name: "Operator",
+        choices:["Addition", "Subtractions", "Multiplication", "Division"],
+    }
+]);
+
+//  conditional statements if-Else
+if(answers.Operator === "Addition"){
+     console.log(answers.firstNumber + answers.secondNumber);
+
+}else if (answers.Operator === "Subtractions"){
+    console.log (answers.firstNumber - answers.secondNumber);
+
+
+}else if (answers.Operator === "Multiplication"){
+    console.log (answers.firstNumber * answers.secondNumber);
+
+   }else if (answers.Operator === "Division"){
+       console.log (answers.firstNumber / answers.secondNumber);
+
+}
+
+
+
+
+
